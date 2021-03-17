69f209a6d8146f2978c2fb2e898aa3357c962701
magnetenstad <tenstad.magne@gmail.com>
2021-03-17 21:14:00 +0100

    Fix readFile()

--- lib/data.dart
+++ lib/data.dart
@@ -3,17 +3,19 @@ import 'package:csv/csv.dart';
 
 const String dataPath = '\\resources\\matvaretabellen.csv';
 
-String readFile(String path) {
-  String contents;
-  File(Directory.current.path + path).readAsString().then((String _contents) {
-    contents = _contents;
+Future<String> readFile(String path) async {
+  return File(Directory.current.path + path)
+      .readAsString()
+      .then((String contents) {
+    return contents;
   });
-  return contents;
 }
 
 void main() async {
-  String csv = readFile(dataPath);
-  List<List<dynamic>> data = const CsvToListConverter().convert(csv);
+  String csv = await readFile(dataPath);
+  List<List<dynamic>> data =
+      const CsvToListConverter().convert(csv, textDelimiter: ';');
+  print(data.length);
   for (List row in data) {
     for (String column in row) {
       print(column);
