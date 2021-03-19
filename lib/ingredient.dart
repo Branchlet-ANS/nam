125ebfe19eedf502ba75947f9511f5fa4fa96f6a
Hans-Marius Øverås <hm@overas.cc>
2021-03-19 17:09:36 +0100

    fixed up ingredient and meal classes

--- lib/ingredient.dart
+++ lib/ingredient.dart
@@ -1,10 +1,22 @@
+import 'dataEnum.dart';
+
 class Ingredient {
   List<dynamic> ingredient;
   double mass;
 
   Ingredient(this.ingredient, this.mass);
 
-  double getNutrientValue(number) {
-    return ingredient[number] * mass / 100;
+  double getNutrientValue(Column number) {
+    if (![Column.ID, Column.Name, Column.Category].contains(number) &&
+        number.index % 2 == 0) {
+      return ingredient[number.index] * mass / 100;
+    } else {
+      throw new FormatException(
+          "Name, Column, Category, Reference are not nutrient values.");
+    }
+  }
+
+  String getName() {
+    return ingredient[Column.Name.index];
   }
 }
--- lib/meal.dart
+++ lib/meal.dart
@@ -1,15 +1,26 @@
+import 'dataEnum.dart';
+import 'ingredient.dart';
+
 class Meal {
-  List<List<dynamic>> ingredients;
+  List<Ingredient> ingredients;
   Meal();
 
   Meal.from(Meal a, Meal b) {
-    ingredients = (a.ingredients + b.ingredients).toSet().toList();
+    ingredients = (a.ingredients + b.ingredients);
   }
-  List<List<dynamic>> getIngredients() {
+  List<Ingredient> getIngredients() {
     return ingredients;
   }
 
-  void addIngredient(List<dynamic> ingredient, double mass) {
+  void addIngredient(Ingredient ingredient, double mass) {
     ingredients.add(ingredient);
+  }
+
+  double getNutrientValue(Column number) {
+    double sum = 0;
+    for (Ingredient ingredient in ingredients) {
+      sum += ingredient.getNutrientValue(number);
+    }
+    return sum;
   }
 }
